777d47d30b7081381498a0bfef352e94ebe9a790
charles <cgiguere@iname.com>
2023-07-07 18:46:56 -0400

    leds

--- ATXPowerSupply.ino
+++ ATXPowerSupply.ino
@@ -1,15 +1,16 @@
-const int leds[] = {2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+
+const int leds[] = {19, 18, 14, 15, 16, 17, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 int ledsCount = sizeof(leds) / sizeof(leds[0]);
 
-void setup(void) {
+void setup(void)
+{
     for (int i = 0; i < ledsCount; i++)
     {
-        pinMode(leds[i],OUTPUT);
-        digitalWrite(leds[i],HIGH);
+        pinMode(leds[i], OUTPUT);
+        digitalWrite(leds[i], HIGH);
     }
-    
 }
 
-void loop(void) {
-
+void loop(void)
+{
 }
