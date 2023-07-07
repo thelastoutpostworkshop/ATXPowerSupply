07af6bf833be5344c26ed8361e5e285904974cf8
charles <cgiguere@iname.com>
2023-07-07 17:13:38 -0400

    test

--- ATXPowerSupply.ino
+++ ATXPowerSupply.ino
@@ -1,7 +1,15 @@
-void setup(void) {
+const int leds[] = {2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+int ledsCount = sizeof(leds) / sizeof(leds[0]);
 
+void setup(void) {
+    for (int i = 0; i < ledsCount; i++)
+    {
+        pinMode(leds[i],OUTPUT);
+        digitalWrite(leds[i],HIGH);
+    }
+    
 }
 
 void loop(void) {
-    
+
 }
